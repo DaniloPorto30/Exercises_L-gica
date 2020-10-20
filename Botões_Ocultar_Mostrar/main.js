fb2f5a9058cce25422adc88f1609732f6d58137d
Danilo Porto <55693717+DaniloPorto30@users.noreply.github.com>
2020-10-19 22:07:30 -0300

    Add files via upload

---
+++ Botões_Ocultar_Mostrar/main.js
@@ -0,0 +1,22 @@
+
+let cor = document.getElementById('paragrafo')
+
+function mudar(){
+
+document.getElementById('frase').style.display='block'
+}
+
+function ler(){
+    cor.style.color  = 'green';
+}
+function ver(){
+    cor.style.color  = '';
+}
+
+function mostrar(){
+    document.getElementById('paragrafo2').style.display='block'
+}
+
+function ocultar(){
+    document.getElementById('paragrafo2').style.display='none'
+}
